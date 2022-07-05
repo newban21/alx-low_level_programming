a40b4e7d9e3f3f2b372a43db7dca488e9180068e
newban21 <fikre2002@gmail.com>
2022-07-05 13:55:58 -0700

    function printed alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,12 +4,12 @@
 *
 * Return: 0
 */
-void print_alphabet(void) 
-{  
+void print_alphabet(void)
+{
 char c;
-for (c = 'a'; c <= 'z'; c++)    
+for (c = 'a'; c <= 'z'; c++)
 {
 _putchar(c);
-}  
+}
 _putchar('\n');
 }
